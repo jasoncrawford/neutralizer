19f08ec3ea3acfe192cbd154bbddb5b5b7bbab43
Jason Crawford <jason@jasoncrawford.org>
2018-11-17 18:31:50 -0800

    tcr

--- spec/syntax_analyzer_spec.rb
+++ spec/syntax_analyzer_spec.rb
@@ -3,9 +3,6 @@ require_relative '../syntax_analyzer'
 describe SyntaxAnalyzer do
   let(:analyzer) { SyntaxAnalyzer.new }
 
-  describe "credentials" do
-  end
-
   describe "client" do
     subject { analyzer.client }
   end
