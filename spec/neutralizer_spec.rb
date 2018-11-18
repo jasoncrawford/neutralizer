3daa2c87a12baf609dcc506bcb7f523ee468427f
Jason Crawford <jason@jasoncrawford.org>
2018-11-17 18:02:39 -0800

    tcr

--- spec/neutralizer_spec.rb
+++ spec/neutralizer_spec.rb
@@ -1,9 +1,10 @@
 require_relative '../neutralizer'
 
 describe Neutralizer do
+  let(:neutralizer) { Neutralizer.new }
+
   describe "neutralize" do
     let(:text) { "foo" }
-    let(:neutralizer) { Neutralizer.new }
     subject { neutralizer.neutralize text }
     it { is_expected.to eql("foo") }
   end
