960f936475278d3c9a5fefc113e970db8473cc32
Jason Crawford <jason@jasoncrawford.org>
2018-11-17 21:46:31 -0800

    tcr

--- neutralizer.rb
+++ neutralizer.rb
@@ -12,9 +12,8 @@ class Neutralizer
     replacements = []
     tokens.each do |token|
       gender = token.part_of_speech.gender
-      if gender == :MASCULINE || gender == :FEMININE
-        replacements << {orig: "He", offset: 0, repl: "They"}
-      end
+      next unless gender == :MASCULINE || gender == :FEMININE
+      replacements << {orig: "He", offset: 0, repl: "They"}
     end
 
     replacements
