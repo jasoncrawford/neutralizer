c3d05abae194512f5dff889959b881914ec86b0b
Jason Crawford <jason@jasoncrawford.org>
2018-11-17 21:28:38 -0800

    tcr

--- neutralizer.rb
+++ neutralizer.rb
@@ -1,10 +1,6 @@
 require 'google/cloud/language'
 
 class Neutralizer
-  def analyze(text)
-    text
-  end
-
   def neutralize(text)
     text
   end
--- spec/neutralizer_spec.rb
+++ spec/neutralizer_spec.rb
@@ -3,12 +3,6 @@ require_relative '../neutralizer'
 describe Neutralizer do
   let(:neutralizer) { Neutralizer.new }
 
-  describe "analyze" do
-    let(:text) { "foo" }
-    subject { neutralizer.analyze text }
-    it { is_expected.to eql("foo") }
-  end
-
   describe "neutralize" do
     let(:text) { "foo" }
     subject { neutralizer.neutralize text }
