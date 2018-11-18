16a2d49b110ca89679d983096df27876c4bc1d41
Jason Crawford <jason@jasoncrawford.org>
2018-11-17 18:39:48 -0800

    tcr

--- syntax_analyzer.rb
+++ syntax_analyzer.rb
@@ -2,7 +2,18 @@ require 'google/cloud/language'
 
 class SyntaxAnalyzer
   def credentials_json
-    {fake: "json"}
+    {
+      "type": "service_account",
+      "project_id": "blind-audition-222804",
+      "private_key_id": ENV['GOOGLE_APPLICATION_PRIVATE_KEY_ID'],
+      "private_key": "fake-private-key",
+      "client_email": "nlp-958@blind-audition-222804.iam.gserviceaccount.com",
+      "client_id": "107848770294889190073",
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+      "token_uri": "https://oauth2.googleapis.com/token",
+      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/nlp-958%40blind-audition-222804.iam.gserviceaccount.com"
+    }
   end
 
   def credentials
