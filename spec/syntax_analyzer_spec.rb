af748fa1c975a3b7925da32602e81d6bcb3950ee
Jason Crawford <jason@jasoncrawford.org>
2018-11-17 18:33:22 -0800

    tcr

--- spec/syntax_analyzer_spec.rb
+++ spec/syntax_analyzer_spec.rb
@@ -4,6 +4,8 @@ describe SyntaxAnalyzer do
   let(:analyzer) { SyntaxAnalyzer.new }
 
   describe "credentials" do
+    subject { analyzer.credentials }
+    it { is_expected.to be_nil }
   end
 
   describe "client" do
