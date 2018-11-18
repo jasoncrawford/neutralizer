040e527473e831a354b50e741695073c5f536033
Jason Crawford <jason@jasoncrawford.org>
2018-11-18 11:23:02 -0800

    tcr

--- syntax_analyzer.rb
+++ syntax_analyzer.rb
@@ -29,6 +29,7 @@ class SyntaxAnalyzer
     dirpath = 'spec/fakes'
     filepath = "#{dirpath}/#{key}.json"
     return nil unless File.exists? filepath
+    puts "returning fake response from #{filepath}"
     json = File.read(filepath)
     Google::Cloud::Language::V1::AnalyzeSyntaxResponse.new(JSON.parse json)
   end
@@ -38,6 +39,7 @@ class SyntaxAnalyzer
     dirpath = 'spec/fakes'
     FileUtils.mkdir_p dirpath
     filepath = "#{dirpath}/#{key}.json"
+    puts "recording fake response in #{filepath}"
     File.open(filepath, "w") {|f| f.write response.to_h.to_json}
   end
 
