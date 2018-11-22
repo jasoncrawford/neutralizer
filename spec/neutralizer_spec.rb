a3c3dbd31db4e984869224820a33935d5bcd58d6
Jason Crawford <jason.crawford@flexport.com>
2018-11-22 13:10:07 -0800

    tcr

--- spec/neutralizer_spec.rb
+++ spec/neutralizer_spec.rb
@@ -97,7 +97,7 @@ describe Neutralizer do
 
       context "two verbs" do
         let(:text) { "She likes the work and wants to do more" }
-        it { is_expected.to be_an(Array) }
+        it { is_expected.to include(*[]) }
       end
 
       context "several gendered words combined" do
