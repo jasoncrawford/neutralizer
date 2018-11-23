1e7b77367f38f1f18cabced6a8a7291def710d46
Jason Crawford <jason.crawford@flexport.com>
2018-11-23 12:56:25 -0800

    tcr

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -14,24 +14,26 @@ describe "server" do
   end
 
   describe "neutralize" do
-    before do
-      post '/neutralize', "She thinks he will"
-    end
+    context "with normal request" do
+      before do
+        post '/neutralize', "She thinks he will"
+      end
 
-    it "should succeed" do
-      expect(last_response).to be_ok
-    end
+      it "should succeed" do
+        expect(last_response).to be_ok
+      end
 
-    it "should allow CORS" do
-      expect(last_response.headers['Access-Control-Allow-Origin']).to eq('*')
-    end
+      it "should allow CORS" do
+        expect(last_response.headers['Access-Control-Allow-Origin']).to eq('*')
+      end
 
-    it "should not allow cache?" do
-      expect(last_response.headers['Cache-Control']).to eq('public, max-age=300')
-    end
+      it "should not allow cache?" do
+        expect(last_response.headers['Cache-Control']).to eq('public, max-age=300')
+      end
 
-    it "should neutralize the body" do
-      expect(last_response.body).to eql('They think they will')
+      it "should neutralize the body" do
+        expect(last_response.body).to eql('They think they will')
+      end
     end
 
     context "with special characters" do
