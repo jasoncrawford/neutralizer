fb7a5e896c6b217689f86d4a6b57d59ea3032c1c
Jason Crawford <jason.crawford@flexport.com>
2018-11-23 14:13:27 -0800

    tcr

--- spec/cache_spec.rb
+++ spec/cache_spec.rb
@@ -1,17 +1,17 @@
 require_relative '../cache'
 
 describe Cache do
-  let(:cache) { Cache.new }
+  before(:all) { @cache = Cache.new }
 
   context "cache miss" do
-    subject { cache.with_cache('foo') { 'bar' } }
+    subject { @cache.with_cache('foo') { 'bar' } }
     it { is_expected.to eq('bar') }
   end
 
   context "cache hit?" do
     before(:all) { @counter = 0 }
 
-    subject { cache.with_cache('foo') { @counter += 1; 'bar' } }
+    subject { @cache.with_cache('foo') { @counter += 1; 'bar' } }
 
     it { is_expected.to eq('bar') }
     it "should not? increment the counter" do
